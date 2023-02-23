14fa70c793fbd2edd508bd4a8c6835f44e251dac
debussysuh <debussysuh@gmail.com>
2023-02-23 22:47:59 +0900

    test: 테스트 케이스 추가

---
+++ src/test/java/com/ll/level0/p120889/Tests.java
@@ -0,0 +1,16 @@
+package com.ll.level0.p120889;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Tests {
+
+    @Test
+    @DisplayName("[1,2,3]이 입력되었을 때의 결과는 2")
+    void t1() {
+        assertThat(new com.ll.level0.p120889.Solution().solution(new int[] {1,2,3})).isEqualTo(2);
+    }
+
+}
